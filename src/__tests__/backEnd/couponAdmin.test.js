dcc3a3b1ce1dccdc8ed08b8089f64464d3af343f
akizyfee <87452790+akizyfee@users.noreply.github.com>
2023-06-03 17:09:00 +0800

    test: coupon 測試

---
+++ src/__tests__/backEnd/couponAdmin.test.js
@@ -0,0 +1,59 @@
+import { test, describe, expect, vi, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { useCouponAdminStore } from '@/stores/backEnd/couponAdmin'
+
+import axios from 'axios'
+
+vi.mock('axios', () => {
+  return {
+    default: {
+      post: vi.fn(),
+      get: vi.fn(),
+      delete: vi.fn(),
+      put: vi.fn(),
+      create: vi.fn().mockReturnThis(),
+      interceptors: {
+        request: {
+          use: vi.fn(),
+          eject: vi.fn()
+        },
+        response: {
+          use: vi.fn(),
+          eject: vi.fn()
+        }
+      }
+    }
+  }
+})
+
+describe('UserAdmin', () => {
+  const getCouponLists = axios.get.mockReturnValue({
+    data: {
+      usersList: [
+        {
+          _id: '6460eb68d1c638b9b291ad28',
+          number: 'A000000001',
+          name: '我是店長',
+          phone: '0912345678',
+          email: 'akizyfee@gmail.com',
+          titleNo: 1,
+          title: '店長',
+          createdAt: '2023/05/14',
+          isDisabled: false
+        }
+      ]
+    }
+  })
+
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  test('優惠碼列表有成功設置 List', async () => {
+    const couponAdminStore = useCouponAdminStore()
+    await couponAdminStore.getCoupon()
+    expect(couponAdminStore.couponList).toBeDefined()
+  })
+  test('Get 方法有被呼叫一次', () => {
+    expect(getCouponLists).toHaveBeenCalledTimes(1)
+  })
+})
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html']
+    }
+  },
   plugins: [
     vue(),
     eslintPlugin({
