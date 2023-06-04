a5507f597f8aa212e829eb780be85b3c9129707b
akizyfee <87452790+akizyfee@users.noreply.github.com>
2023-06-04 20:45:23 +0800

    fix: loadStore 命名

--- src/stores/TheLoading.js
+++ src/stores/TheLoading.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useLoadingStore = defineStore('seatData', () => {
+export const useLoadingStore = defineStore('loadingData', () => {
   const isLoading = ref(false)
   return {
     isLoading
