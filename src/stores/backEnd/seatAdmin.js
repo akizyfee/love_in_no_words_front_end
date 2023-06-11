fe14e0d46a99690e269d74d867b15d1393046313
akizyfee <87452790+akizyfee@users.noreply.github.com>
2023-06-11 14:37:34 +0800

    feat: admin 座位 loading

--- src/stores/backEnd/seatAdmin.js
+++ src/stores/backEnd/seatAdmin.js
@@ -3,18 +3,24 @@ import { defineStore } from 'pinia'
 import { catchError } from '@/utils/catchError'
 import { warningAlert, successAlert } from '@/plugins/toast'
 import { getAdminSeat, addAdminSeat, editAdminSeat, deleteAdminSeat } from '@/apis/seat'
+import { useLoadingStore } from '@/stores/TheLoading'
 
 export const useSeatAdminStore = defineStore('seatAdminData', () => {
+  const loding = useLoadingStore()
+
   /**
    * 取得座位列表
    **/
   const seatList = ref([])
   const getSeats = catchError(async () => {
+    loding.isLoading = true
     const { data } = await getAdminSeat()
     if (data.length === 0) {
       warningAlert('尚未建立座位的資料')
+      loding.isLoading = false
     }
     seatList.value = data
+    loding.isLoading = false
   })
 
   /**
