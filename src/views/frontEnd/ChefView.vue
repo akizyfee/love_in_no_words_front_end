19f17da569675ed0e8c1fde1f4227a000379ed5f
akizyfee <87452790+akizyfee@users.noreply.github.com>
2023-06-11 13:54:50 +0800

    feat/chef

--- src/views/frontEnd/ChefView.vue
+++ src/views/frontEnd/ChefView.vue
@@ -2,33 +2,80 @@
 import SiderBar from '@/components/frontEnd/SideBar.vue'
 import { io } from 'socket.io-client'
 import { successAlert, warningAlert } from '@/plugins/toast'
+import { onMounted, ref, watch } from 'vue'
+
+import { searchPickUp, editPickUp } from '@/apis/order'
+import { catchError } from '@/utils/catchError'
 
 const socket = io(import.meta.env.VITE_SOCKET_URL)
 
 /**
- * 發送出餐訊息
- * employee 為店員
- * chef 為廚師
- * @param {object} msg 訊息
- */
-const submitEmployee = () => {
-  const sendMessage = {
-    message: '訂單XXX已製作完成，請出餐'
-  }
-  socket.emit('employee', sendMessage)
-}
-
-/**
  * 接收訊息
  */
-socket.on('employee', (messages) => {
-  warningAlert(messages.message)
-})
 socket.on('chef', (messages) => {
   // 這邊等 API 串接收到消息可以改成刷新訂單，就不用通知了
   if (messages) {
-    successAlert('收到訂單')
+    warningAlert('收到前台點餐訂單')
+    fetchSearchPickUp()
   }
+})
+
+/**
+ * 出餐時間計算
+ */
+const useProductionTimeCaculater = (data) => {
+  return data.map((temp) => {
+    const totalTime = temp.orderList
+      .map((item) => {
+        return item.qty * item.productionTime
+      })
+      .reduce((acc, cur) => acc + cur, 0)
+    return { ...temp, totalTime }
+  })
+}
+
+/**
+ * 查詢訂單
+ */
+const selectStatus = ref(['未出餐', '已出餐'])
+const currentStatus = ref('未出餐')
+const pickUpList = ref([])
+const fetchSearchPickUp = catchError(async () => {
+  const { data } = await searchPickUp(currentStatus.value)
+  const newDessertList = useProductionTimeCaculater(data)
+  pickUpList.value = newDessertList
+})
+
+onMounted(() => {
+  fetchSearchPickUp()
+})
+
+watch(
+  [() => currentStatus],
+  () => {
+    fetchSearchPickUp()
+  },
+  {
+    immediate: true,
+    deep: true
+  }
+)
+
+/**
+ * 出餐
+ */
+const overStatus = ref({
+  status: '已出餐'
+})
+const fetchSendPickUp = catchError(async (oderId) => {
+  const { data } = await editPickUp(oderId, overStatus.value)
+  successAlert(data.status)
+
+  const sendMessage = {
+    message: `'訂單 ${oderId} 已製作完成，請出餐'`
+  }
+  socket.emit('employee', sendMessage)
+  fetchSearchPickUp()
 })
 </script>
 <template>
@@ -43,78 +90,56 @@ socket.on('chef', (messages) => {
     <main class="min-h-screen p-6">
       <!-- menu -->
       <ul class="flex text-xl font-medium text-center break-keep overflow-x-auto mb-6">
-        <li class="mr-2">
+        <li v-for="(select, key) in selectStatus" :key="key" class="mr-2">
           <a
-            href="#"
-            class="block px-6 py-3 rounded-lg hover:text-primary-light hover:bg-white tabbar-active"
-            >未出餐</a
-          >
-        </li>
-        <li class="mr-2">
-          <a href="#" class="block px-6 py-3 rounded-lg hover:text-primary-light hover:bg-white"
-            >已出餐</a
+            @click.prevent="currentStatus = select"
+            class="block px-6 py-3 rounded-lg hover:text-primary-light hover:bg-white"
+            :class="{ 'tabbar-active': currentStatus === select }"
+            >{{ select }}</a
           >
         </li>
       </ul>
       <!-- table -->
-      <ul class="grid grid-cols-12 gap-4">
+      <ul class="overflow-x-auto flex space-x-2">
         <li
-          class="col-span-12 lg:col-span-6 xl:col-span-4 bg-white border-2 border-textself rounded-lg shadow"
+          v-for="item in pickUpList"
+          :key="item.orderNo"
+          class="min-w-[300px] h-[600px] bg-white flex flex-col justify-between border-2 border-textself rounded-lg shadow"
         >
-          <div class="flex justify-between px-2 pt-4 rounded-lg bg-bgself-light">
-            <p class="text-lg font-medium text-white bg-secondary-light rounded-lg py-1 px-2">
-              訂單編號 : 123456789
-            </p>
+          <div>
+            <div class="flex justify-between px-2 pt-4 rounded-lg bg-bgself-light">
+              <p class="text-lg font-medium text-white bg-secondary-light rounded-lg py-1 px-2">
+                訂單編號 : {{ item.orderNo }}
+              </p>
+            </div>
+            <div class="flex justify-between p-4 bg-bgself-light">
+              <p class="font-medium text-xl text-primary-light">預計製作時間</p>
+              <p class="font-medium text-xl text-primary-light">{{ item.totalTime }} 分鐘</p>
+            </div>
+            <!-- cartList -->
+            <ul class="flex flex-col overflow-y-auto max-h-[400px]">
+              <li
+                v-for="(dessert, index) in item.orderList"
+                :key="`${index}${dessert.productName}`"
+                class="flex justify-between items-center border-t border-textself p-4"
+              >
+                <div>
+                  <h2 class="font-medium text-xl">
+                    <span>{{ dessert.productName }}</span>
+                    <span>&emsp;x{{ dessert.qty }}</span>
+                  </h2>
+                  <p class="font-medium text-xl text-primary-light my-2">備註訊息：後續補上</p>
+                </div>
+              </li>
+            </ul>
           </div>
-          <div class="flex justify-between p-4 bg-bgself-light">
-            <p class="font-medium text-xl text-primary-light">預計出餐時間</p>
-            <p class="font-medium text-xl text-primary-light">12:30</p>
-          </div>
-          <!-- cartList -->
-          <ul class="flex flex-col">
-            <li class="flex justify-between items-center border-t border-textself p-4">
-              <div>
-                <h2 class="font-medium text-xl">
-                  <span>香草鮮奶酪</span>
-                  <span>&emsp;x1</span>
-                </h2>
-                <p class="font-medium text-xl text-primary-light my-2">備註訊息：無</p>
-              </div>
-            </li>
-            <li class="flex justify-between items-center border-t border-textself p-4">
-              <div>
-                <h2 class="font-medium text-xl">
-                  <span>巧克力糖霜甜甜圈</span>
-                  <span>&emsp;x2</span>
-                </h2>
-                <p class="font-medium text-xl text-primary-light my-2">備註訊息：無</p>
-              </div>
-            </li>
-            <li class="flex justify-between items-center border-t border-textself p-4">
-              <div>
-                <h2 class="font-medium text-xl">
-                  <span>香草鮮奶酪</span>
-                  <span>&emsp;x1</span>
-                </h2>
-                <p class="font-medium text-xl text-primary-light my-2">備註訊息：無</p>
-              </div>
-            </li>
-            <li class="flex justify-between items-center border-t border-textself p-4">
-              <div>
-                <h2 class="font-medium text-xl">
-                  <span>巧克力糖霜甜甜圈</span>
-                  <span>&emsp;x2</span>
-                </h2>
-                <p class="font-medium text-xl text-primary-light my-2">備註訊息：無</p>
-              </div>
-            </li>
-          </ul>
           <!-- send_btn -->
           <div
             class="rounded-br-lg rounded-bl-lg bg-bgself-light hover:bg-primary-light hover:text-white text-center"
           >
             <button
-              @click.prevent="submitEmployee"
+              v-if="item.status === '未出餐'"
+              @click.prevent="fetchSendPickUp(item.orderNo)"
               type="button"
               class="font-medium text-xl w-full p-4"
             >
